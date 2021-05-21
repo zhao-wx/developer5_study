3c08380a73dfce3f35eae00b61647adf646d1bd0
caoqs <3157570025@qq.com>
2021-05-21 10:03:10 +0800

    钟婷婷土狗

---
+++ maven_ssm/src/main/java/com/ck/test/test2.java
@@ -0,0 +1,12 @@
+package com.ck.test;
+
+/**
+ * @Descrition:
+ * @Author caoqs
+ * @Date 2021/5/21 10:00
+ */
+public class test2 {
+    public static void main(String[] args) {
+        System.out.println("钟婷婷傻逼呢");
+    }
+}
